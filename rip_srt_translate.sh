659e369574ba2e2280a0f60557a16d3cca279593
Tina Yao <74569724+Ti-Yao@users.noreply.github.com>
2021-06-22 18:43:31 +0100

    Update rip_srt_translate.sh

--- rip_srt_translate.sh
+++ rip_srt_translate.sh
@@ -1,5 +1,5 @@
-for file in *.mkv; do
+for file in *.mkv; do # change mkv to video format of choice
 echo ${file##*/}
-#ffmpeg -i "./${file##*/}" "${file%.*}.srt"  #rip
+ffmpeg -i "./${file##*/}" "${file%.*}.srt"  #rip subtitle track
 python3 translator.py "${file%.*}" #translate
 done
