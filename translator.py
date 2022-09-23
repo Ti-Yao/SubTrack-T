64a7f0f87dcc90669ae6bd1bfc5781bc353c1d8d
Tina Yao <74569724+Ti-Yao@users.noreply.github.com>
2022-09-23 12:00:46 +0100

    Update translator.py

--- translator.py
+++ translator.py
@@ -7,12 +7,23 @@ import sys
 
 translator = Translator()
 NAME = sys.argv[1]
-file = open(r"{}.srt".format(NAME,"r"))
+file = open(f"{NAME}.srt","r")
 subs = list(srt.parse(file))
+merge = True
+
 for i in range(len(subs)):
     line = subs[i].content
-    current_translate = translator.translate(str(line), dest='zh-CN').text.encode('utf-8').decode('utf-8')
-    subs[i].content = current_translate
-fo = open(r"{0}.srt".format(NAME),"wb")
+    translated_line = translator.translate(str(line), dest='zh-CN').text.encode('utf-8').decode('utf-8').replace('\n','')
+    if merge:
+        subs[i].content = line + '\n' + translated_line
+    else:
+        subs[i].content = translated_line
+
+
+if merge:
+    output_file = f"{NAME}_merge.srt"
+else:
+    output_file = f"{NAME}_cn.srt"
+fo = open(output_file,"wb")
 fo.write(srt.compose(subs).encode('utf8'))
 fo.close()
